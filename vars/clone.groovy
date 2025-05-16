4a48b75040771003ec3edff48f63315dfb17a501
aayushITT <aayush.s@intimetec.com>
2025-05-16 14:49:30 +0530

    Update clone.groovy

--- vars/clone.groovy
+++ vars/clone.groovy
@@ -1,5 +1,4 @@
-def call(String url , String branch)
-{
-   git url: ${url},branch:${branch}
-  echo "cloning successfull from shared libraries"
+def call(String url, String branch) {
+    git url: url, branch: branch
+    echo "Cloning successful from shared libraries"
 }
